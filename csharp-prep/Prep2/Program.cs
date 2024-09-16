dfb8e4ef03964ecc3693eeaee52029541a1640fa
NHorwath <nicolasadhorwath@gmail.com>
2024-09-16 15:19:52 -0300

    Prep2 basic code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string ValueFromUser = Console.ReadLine();
+
+        int GradePercentage = int.Parse(ValueFromUser);
+        string letter = "F";
+
+        if (GradePercentage >= 90)
+        {
+           letter = "A";
+        }
+
+        else if (GradePercentage >= 80)
+        {
+           letter = "B";
+        }
+
+        else if (GradePercentage >= 70)
+        {
+           letter = "C";
+        }
+
+        else if (GradePercentage >= 60)
+        {
+           letter = "D";
+        }
+
+        else
+        {
+           letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+        Console.WriteLine("");
     }
 }
