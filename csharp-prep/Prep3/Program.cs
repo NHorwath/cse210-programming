05055c708855c2164880cbe5a56c20383374d0c2
NHorwath <nicolasadhorwath@gmail.com>
2024-09-16 17:25:10 -0300

    Prep3 Final code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {  
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        Console.WriteLine($"Guess the number: {number}");
+        int UserNumber = 0;
+
+        do
+        {
+            Console.Write("What is your number? ");
+            string UserInput = Console.ReadLine();
+            UserNumber = int.Parse(UserInput);
+
+            if (UserNumber < number)
+            {
+                Console.WriteLine("Higher");
+                Console.WriteLine("");
+            }
+
+            else if (UserNumber > number)
+            {
+                Console.WriteLine("Lower");
+                Console.WriteLine("");
+            }
+        } while (UserNumber != number);
+
+        Console.WriteLine("You guessed it!");
     }
 }
