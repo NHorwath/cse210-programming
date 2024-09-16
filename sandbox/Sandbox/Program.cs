41aada3527907546fe6547bc49381a8b1624a870
NHorwath <nicolasadhorwath@gmail.com>
2024-09-16 14:22:28 -0300

    Testing code on Sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}.");
     }
+    
 }
