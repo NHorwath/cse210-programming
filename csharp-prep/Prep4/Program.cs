27b71c950f0820700fab6ee9e70cd62f5cabf120
NHorwath <nicolasadhorwath@gmail.com>
2024-09-17 14:49:21 -0300

    Prep4 final Code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,54 @@
 using System;
-
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers = new List<int>();
+
+        int LargestNumber = -1;
+        int UserNumbers = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+        
+
+        do
+        {
+            Console.Write("What is your number? ");
+            string UserInput = Console.ReadLine();
+            UserNumbers = int.Parse(UserInput);
+
+            if (UserNumbers != 0)
+            {
+                numbers.Add(UserNumbers);
+            }
+
+            if (LargestNumber < UserNumbers)
+            {
+                LargestNumber = UserNumbers;
+            }
+
+        } while (UserNumbers != 0);
+
+        int TotalNumbers = numbers.Count;
+        var Sum = numbers.Sum();
+        var Avg = Sum / TotalNumbers;
+
+
+
+        Console.WriteLine("");
+        Console.WriteLine($"The sum is: {Sum}");
+        Console.WriteLine($"The average is: {Avg}");
+        Console.WriteLine($"The largest number is: {LargestNumber}");
     }
+
+    
+
+
+
+
+
+
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,10 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        Console.Write("What is your favorite color? ");
-        string color = Console.ReadLine();
-        Console.WriteLine($"Your favorite color is {color}.");
+        List<string> words = new List<string>();
+
+        words.Add("phone");
+        words.Add("keyboard");
+        words.Add("mouse");
+
+        for (int i = 0; i < words.Count; i++)
+        {
+            Console.WriteLine(words[i]);
+        }
+
     }
     
 }
